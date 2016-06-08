45c66df0970df4ea1bc56e23be03bcde3399c119
Daniel Steuernol <dlsteuer@gmail.com>
2016-06-07 22:35:02 -0700

    Sample Winforms application to report to Rollbar.

---
+++ RollbarWinformsSample/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using RollbarDotNet;
+using Exception = System.Exception;
+
+namespace RollbarWinformsSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs args)
+        {
+            try
+            {
+                List<string> foo = null;
+                foo.Add("test");
+            }
+            catch (Exception e)
+            {
+                Rollbar.Report(e);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<string> foo = null;
+            foo.Add("test");
+        }
+    }
+}
---
+++ RollbarWinformsSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using RollbarDotNet;
+
+namespace RollbarWinformsSample
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Rollbar.Init(new RollbarConfig
+            {
+                AccessToken = "POST_SERVER_ACCESS_TOKEN",
+                Environment = "production"
+            });
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.ThreadException += (sender, args) =>
+            {
+                Rollbar.Report(args.Exception);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Rollbar.Report(args.ExceptionObject as System.Exception);
+            };
+
+            Application.Run(new Form1());
+        }
+    }
+}
